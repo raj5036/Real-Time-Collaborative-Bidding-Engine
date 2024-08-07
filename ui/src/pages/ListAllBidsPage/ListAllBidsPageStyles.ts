39ddb4159d38c3ff557bff30399b8b1a75a4d217
Raj Karmakar <97rajmath@gmail.com>
2024-08-07 17:56:28 +0530

    Integrated Material UI table into the app

--- ui/src/pages/ListAllBidsPage/ListAllBidsPageStyles.ts
+++ ui/src/pages/ListAllBidsPage/ListAllBidsPageStyles.ts
@@ -1,8 +1,10 @@
-import { Box } from "@mui/material";
+import { Box, List } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 export const MainContainer = styled(Box)(() => `
 	text-align: center;
 `)
 
-export const ListContainer = styled(Box)(() => ``)
+export const ListContainer = styled(List)(({ theme }) => `
+	margin-top: ${theme.spacing(5)};
+`)
