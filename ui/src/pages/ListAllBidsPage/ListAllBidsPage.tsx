8a7bb01ae15829eac68bc8834a93942dba5591aa
Raj Karmakar <97rajmath@gmail.com>
2024-08-07 18:45:20 +0530

    Show fetched details on Table

--- ui/src/pages/ListAllBidsPage/ListAllBidsPage.tsx
+++ ui/src/pages/ListAllBidsPage/ListAllBidsPage.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
@@ -20,6 +20,9 @@ import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
+import { GetAllBidsByUser } from '../../utils/ApiClient';
+import { toast } from 'react-toastify';
+import { IBidItem } from '../../utils/Types';
 
 interface Data {
 	id: number,
@@ -49,20 +52,14 @@ function createData(
   };
 }
 
-const rows = [
-  createData(1, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(2, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(3, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(4, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(5, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(6, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(7, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(8, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(9, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(10, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(11, 'Cupcake', '305', '3.7', '67', '4.3'),
-  createData(12, 'Cupcake', '305', '3.7', '67', '4.3'),
-];
+type BidData = {
+	title: string,
+	startDate: string,
+	startTime: string,
+	endDate: string,
+	endTime: string,
+	bidItems: IBidItem[]
+}
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
@@ -258,163 +255,183 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
   );
 }
 export default function ListAddBidsPage() {
-  const [order, setOrder] = React.useState<Order>('asc');
-  const [orderBy, setOrderBy] = React.useState<keyof Data>('title');
-  const [selected, setSelected] = React.useState<readonly number[]>([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Data,
-  ) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
+	const [bids, setBids] = useState<BidData[]>([])
+	const [rows, setRows] = useState<Data[]>([])
+	const [order, setOrder] = useState<Order>('asc');
+	const [orderBy, setOrderBy] = useState<keyof Data>('title');
+	const [selected, setSelected] = useState<readonly number[]>([]);
+	const [page, setPage] = useState(0);
+	const [dense, setDense] = useState(false);
+	const [rowsPerPage, setRowsPerPage] = useState(5);
 
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.id);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
+	useEffect(() => {
+		(async () => {
+			const result = await GetAllBidsByUser()
+			if (result.success) {
+				console.log(result)
+				setBids(() => {
+					const newRows = result.bids.map((b: BidData, i: number) => createData(i, b.title, b.startDate, b.startTime, b.endDate, b.endTime))
+					setRows(newRows)
+					console.log(bids)
+					return result
+				})
+			} else {
+				toast.error("Something went wrong! Please refresh the Page")
+			}
+		})()
+	}, [])
 
-  const handleClick = (event: React.MouseEvent<unknown>, id: number) => {
-    const selectedIndex = selected.indexOf(id);
-    let newSelected: readonly number[] = [];
+	const handleRequestSort = (
+		event: React.MouseEvent<unknown>,
+		property: keyof Data,
+	) => {
+		const isAsc = orderBy === property && order === 'asc';
+		setOrder(isAsc ? 'desc' : 'asc');
+		setOrderBy(property);
+	};
 
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, id);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-    setSelected(newSelected);
-  };
+	const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.checked) {
+		const newSelected = rows.map((n) => n.id);
+		setSelected(newSelected);
+		return;
+		}
+		setSelected([]);
+	};
 
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
+	const handleClick = (event: React.MouseEvent<unknown>, id: number) => {
+		const selectedIndex = selected.indexOf(id);
+		let newSelected: readonly number[] = [];
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+		if (selectedIndex === -1) {
+		newSelected = newSelected.concat(selected, id);
+		} else if (selectedIndex === 0) {
+		newSelected = newSelected.concat(selected.slice(1));
+		} else if (selectedIndex === selected.length - 1) {
+		newSelected = newSelected.concat(selected.slice(0, -1));
+		} else if (selectedIndex > 0) {
+		newSelected = newSelected.concat(
+			selected.slice(0, selectedIndex),
+			selected.slice(selectedIndex + 1),
+		);
+		}
+		setSelected(newSelected);
+	};
 
-  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDense(event.target.checked);
-  };
+	const handleChangePage = (event: unknown, newPage: number) => {
+		setPage(newPage);
+	};
 
-  const isSelected = (id: number) => selected.indexOf(id) !== -1;
+	const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setRowsPerPage(parseInt(event.target.value, 10));
+		setPage(0);
+	};
+
+	const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDense(event.target.checked);
+	};
+
+	const isSelected = (id: number) => selected.indexOf(id) !== -1;
 
   // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+	const emptyRows =
+		page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
-  const visibleRows = React.useMemo(
-    () =>
-      stableSort(rows, getComparator(order, orderBy)).slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage,
-      ),
-    [order, orderBy, page, rowsPerPage],
-  );
+	const visibleRows = React.useMemo(
+		() =>
+		stableSort(rows, getComparator(order, orderBy)).slice(
+			page * rowsPerPage,
+			page * rowsPerPage + rowsPerPage,
+		),
+		[rows, order, orderBy, page, rowsPerPage],
+	);
 
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
-          >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
-            />
-            <TableBody>
-              {visibleRows.map((row, index) => {
-                const isItemSelected = isSelected(row.id);
-                const labelId = `enhanced-table-checkbox-${index}`;
+	return (
+		<Box sx={{ width: '100%' }}>
+		<Paper sx={{ width: '100%', mb: 2 }}>
+			<EnhancedTableToolbar numSelected={selected.length} />
+			<TableContainer>
+			<Table
+				sx={{ minWidth: 750 }}
+				aria-labelledby="tableTitle"
+				size={dense ? 'small' : 'medium'}
+			>
+				<EnhancedTableHead
+				numSelected={selected.length}
+				order={order}
+				orderBy={orderBy}
+				onSelectAllClick={handleSelectAllClick}
+				onRequestSort={handleRequestSort}
+				rowCount={rows.length}
+				/>
+				<TableBody>
+				{visibleRows.map((row, index) => {
+					const isItemSelected = isSelected(row.id);
+					const labelId = `enhanced-table-checkbox-${index}`;
 
-                return (
-                  <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, row.id)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.id}
-                    selected={isItemSelected}
-                    sx={{ cursor: 'pointer' }}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        color="primary"
-                        checked={isItemSelected}
-                        inputProps={{
-                          'aria-labelledby': labelId,
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                    //   padding="none"
-                    >
-                      {row.id}
-                    </TableCell>
-                    <TableCell align="left">{row.title}</TableCell>
-                    <TableCell align="left">{row.startDate}</TableCell>
-                    <TableCell align="left">{row.startTime}</TableCell>
-                    <TableCell align="left">{row.endDate}</TableCell>
-                    <TableCell align="left">{row.endTime}</TableCell>
-                  </TableRow>
-                );
-              })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
-    </Box>
-  );
+					return (
+					<TableRow
+						hover
+						onClick={(event) => handleClick(event, row.id)}
+						role="checkbox"
+						aria-checked={isItemSelected}
+						tabIndex={-1}
+						key={row.id}
+						selected={isItemSelected}
+						sx={{ cursor: 'pointer' }}
+					>
+						<TableCell padding="checkbox">
+						<Checkbox
+							color="primary"
+							checked={isItemSelected}
+							inputProps={{
+							'aria-labelledby': labelId,
+							}}
+						/>
+						</TableCell>
+						<TableCell
+						component="th"
+						id={labelId}
+						scope="row"
+						//   padding="none"
+						>
+						{row.id}
+						</TableCell>
+						<TableCell align="left">{row.title}</TableCell>
+						<TableCell align="left">{row.startDate}</TableCell>
+						<TableCell align="left">{row.startTime}</TableCell>
+						<TableCell align="left">{row.endDate}</TableCell>
+						<TableCell align="left">{row.endTime}</TableCell>
+					</TableRow>
+					);
+				})}
+				{emptyRows > 0 && (
+					<TableRow
+					style={{
+						height: (dense ? 33 : 53) * emptyRows,
+					}}
+					>
+					<TableCell colSpan={6} />
+					</TableRow>
+				)}
+				</TableBody>
+			</Table>
+			</TableContainer>
+			<TablePagination
+			rowsPerPageOptions={[5, 10, 25]}
+			component="div"
+			count={rows.length}
+			rowsPerPage={rowsPerPage}
+			page={page}
+			onPageChange={handleChangePage}
+			onRowsPerPageChange={handleChangeRowsPerPage}
+			/>
+		</Paper>
+		<FormControlLabel
+			control={<Switch checked={dense} onChange={handleChangeDense} />}
+			label="Dense padding"
+		/>
+		</Box>
+	);
 }
